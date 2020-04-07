fec643500b9e1b00c62d75cf012d9f580cdd6279
Sayantan Barik <sayantanbarik729@gmail.com>
2020-04-07 19:30:02 +0530

    none

--- app/src/main/java/com/example/rewardanimation/Dialogs/CongratsDialog.java
+++ app/src/main/java/com/example/rewardanimation/Dialogs/CongratsDialog.java
@@ -19,35 +19,42 @@ import com.airbnb.lottie.LottieAnimationView;
 import com.example.rewardanimation.R;
 
 public class CongratsDialog extends DialogFragment {
-    private LottieAnimationView lottieAnimationView;
-    private TextView textView;
-    private Animation zoomout;
+    private LottieAnimationView levelup,achievement;
+    private TextView level,nachievement;
+    private Animation zoomout1,zoomout2,bounce;
     private SharedPreferences sharedPreferences;
 
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View view=inflater.inflate(R.layout.congratsdialog,container,false);
-
-        lottieAnimationView=view.findViewById(R.id.congrats);
-        textView=view.findViewById(R.id.level);
+        levelup=view.findViewById(R.id.congrats);
+        achievement=view.findViewById(R.id.achieve);
+        level=view.findViewById(R.id.level);
+        nachievement=view.findViewById(R.id.nachieve);
         sharedPreferences=getContext().getSharedPreferences("MyPrefs", Context.MODE_PRIVATE);
-        textView.setText("Level "+sharedPreferences.getInt("Level",1));
+        level.setText("Level "+sharedPreferences.getInt("Level",1));
+        levelup.setAnimation(R.raw.levelup);
+        achievement.setAnimation(R.raw.correct);
+        achievement.setRepeatCount(Animation.INFINITE);
+        achievement.playAnimation();
+        zoomout1= AnimationUtils.loadAnimation(getContext(),R.anim.zoomout);
+        zoomout2= AnimationUtils.loadAnimation(getContext(),R.anim.zoomout);
+        bounce=AnimationUtils.loadAnimation(getContext(),R.anim.bounce);
 
-        lottieAnimationView.setAnimation(R.raw.levelup);
-        zoomout= AnimationUtils.loadAnimation(getContext(),R.anim.zoomout);
-        zoomout.setAnimationListener(new Animation.AnimationListener() {
+        nachievement.setText("New Achievement");
+        nachievement.setVisibility(View.VISIBLE);
+        nachievement.startAnimation(zoomout2);
+        zoomout2.setAnimationListener(new Animation.AnimationListener() {
             @Override
             public void onAnimationStart(Animation animation) {
-
 
             }
 
             @Override
             public void onAnimationEnd(Animation animation) {
-
-
-
+                levelup.setVisibility(View.VISIBLE);
+                levelup.playAnimation();
             }
 
             @Override
@@ -55,7 +62,7 @@ public class CongratsDialog extends DialogFragment {
 
             }
         });
-        lottieAnimationView.addAnimatorListener(new Animator.AnimatorListener() {
+        levelup.addAnimatorListener(new Animator.AnimatorListener() {
             @Override
             public void onAnimationStart(Animator animator) {
 
@@ -64,11 +71,13 @@ public class CongratsDialog extends DialogFragment {
 
             @Override
             public void onAnimationEnd(Animator animator) {
-                textView.startAnimation(zoomout);
+                levelup.startAnimation(bounce);
+                level.startAnimation(zoomout1);
                 SharedPreferences.Editor editor=sharedPreferences.edit();
                 editor.putInt("Level",sharedPreferences.getInt("Level",1)+1);
                 editor.apply();
-                textView.setText("Level "+sharedPreferences.getInt("Level",1));
+                level.setText("Level "+sharedPreferences.getInt("Level",1));
+
 
 
 
@@ -86,7 +95,7 @@ public class CongratsDialog extends DialogFragment {
 
             }
         });
-        lottieAnimationView.playAnimation();
+
 
         return view;
     }
